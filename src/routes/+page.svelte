f8db7254d53157afd0ae6155a445dc263f1c70dd
carvguilherme8 <cguilherme182@gmail.com>
2025-04-27 15:17:53 -0300

    add titulo e paragrafo de explicação

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>Tarefa 4 - Olimpíadas</title>
+</svelte:head>
+
+<div class='title'>
+<h1>🏆  Análise de Resultados - Olimpíadas 🏅</h1> 
+<h3>guilherme buss e guilherme carvalho</h3>
+<p>A seguinte visualização tem como objetivo analisar a evolução do desempenho de atletas olímpicos com o passar dos anos. 
+    Utilizamos o seguinte dataset: <a href="https://www.kaggle.com/datasets/piterfm/olympic-games-medals-19862018?select=olympic_results.csv" target="_blank" rel="noopener noreferrer"class="link-dataset"> Olympic Results (1986-2018) </a>, 
+    que contém os resultados (tempo, distânica, peso) de diversas modalidades no intervalo de 1986 até 2018, contendo também o nome 
+    dos atletas que realizaram o feito.</p>
+</div>
+
+
+<style>
+    .title {
+        font-size: 35px;
+        text-align: Center;
+        margin: 0;
+    }
+    h1 {
+        font-family:monospace;
+        font-weight: 100;
+        font-size: 1em;
+        letter-spacing: 1.3px;
+    }
+    h3 {
+        font-family:monospace;
+        font-weight: 100;
+        font-size: 0.6em;
+        letter-spacing: 0.3px;
+    }
+    p {
+        font-size: 0.5em;
+        width: 70%;
+        margin: 0 auto;
+        text-align: center;
+    }
+  </style>
